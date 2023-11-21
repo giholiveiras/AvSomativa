5c84566bc9dc4482e46af8b64bd3a9cd45476add
giholiveiras <132359809+giholiveiras@users.noreply.github.com>
2023-11-21 09:39:57 -0300

    Add files via upload

---
+++ AvSomativa/EmpresaJWER/src/main/java/com/projetojpa/controller/CursoController.java
@@ -0,0 +1,84 @@
+package com.projetojpa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetojpa.entities.Curso;
+import com.projetojpa.services.CursoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@Tag (name="curso", description = "API REST DE GERENCIAMENTO DE CURSOS")
+@RestController
+@RequestMapping("/curso")
+public class CursoController {
+	private final CursoService CursoService;
+
+	@Autowired
+	public CursoController(CursoService cursoService) {
+		this.CursoService = cursoService;
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary = "Localiza Curso por iD")
+	public ResponseEntity<Curso> buscaCursoControlId(@PathVariable Long id) {
+		Curso curso = CursoService.buscaCursoId(id);
+		if (curso != null) {
+			return ResponseEntity.ok(curso);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	@GetMapping("/")
+	@Operation(summary = "Apresenta todos os Cursos")
+	public ResponseEntity<List<Curso>> buscaTodosCursoControl() {
+		List<Curso> Curso = CursoService.buscaTodosCursos();
+		return ResponseEntity.ok(Curso);
+	}
+
+	@PostMapping ("/")
+	@Operation(summary = "Cadastra um Curso")
+	public ResponseEntity<Curso> salvaCursoControl (@RequestBody @Valid Curso curso){
+		Curso salvaCurso = CursoService.salvarCurso(curso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(salvaCurso);
+	}
+	
+	@PutMapping("/{id}")
+	@Operation(summary = "Altera um Curso")
+	public ResponseEntity<Curso> alteraCursoControl (@PathVariable Long id, @RequestBody @Valid Curso curso){
+		Curso alteraCurso = CursoService.alterarCurso(id, curso);
+		if (alteraCurso != null) {
+			return ResponseEntity.ok(curso);
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	@DeleteMapping("{id}")
+	@Operation(summary = "Exclui um Curso")
+	public ResponseEntity<Curso> apagaCursoControl(@PathVariable Long id){
+		boolean apagar = CursoService.apagarCurso(id);
+		if(apagar) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
+
+
